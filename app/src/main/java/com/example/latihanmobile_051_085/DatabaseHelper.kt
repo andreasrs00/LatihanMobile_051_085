8a00a0518ebe8db14a7540eff8bb51cfd7135617
Andreas Robson Simanjuntak <xandersimanjuntak@gmail.com>
2024-11-28 15:46:53 +0700

    2 dn 4

---
+++ app/src/main/java/com/example/latihanmobile_051_085/DatabaseHelper.kt
@@ -0,0 +1,24 @@
+package com.example.latihanmobile_051_085
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+internal class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_NAME = "dbhomework"
+        private const val DATABASE_VERSION = 1
+        private const val SQL_CREATE_TABLE_NOTE = "CREATE TABLE $TABLE_NAME" +
+        " ($(HoneworkColumns._ID) INTEGER PRIMARY KEY AUTOINCREMENT," +
+        " $(HoneworkColumns.TITLE) TEXT NOT NULL," +
+        " $(HoneworkColumns.DESCRIPTION) TEXT NOT NULL," +
+        " $(HoneworkColumns.DATE) TEXT NOT NULL)"
+    }
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_TABLE_NOTE)
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+}
--- app/src/main/java/com/example/latihanmobile_051_085/Homework.kt
+++ app/src/main/java/com/example/latihanmobile_051_085/Homework.kt
@@ -1,3 +1,8 @@
 package com.example.latihanmobile_051_085
 
-data class Homework()
+data class Homework(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var date: String? = null,
+)
